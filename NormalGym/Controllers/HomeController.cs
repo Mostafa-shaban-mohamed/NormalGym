96372057031ff9337ace9e3e85dd811253933e6f
Mostafa Shaaban Mohamed <51713741+Mostafa-shaban-mohamed@users.noreply.github.com>
2020-08-25 14:17:32 +0200

    Semi-final Push

---
+++ NormalGym/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NormalGym.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            return PartialView("_Schedule");
+        }
+
+        public ActionResult About()
+        {
+            return PartialView("About");
+        }
+
+        public ActionResult Contact()
+        {
+            return PartialView("Contact");
+        }
+
+        public ActionResult _Home()
+        {
+            return PartialView("_Home");
+        }
+    }
+}
---
+++ NormalGym/scripts/NavBarScript.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    //Default view
+    $("#myContentView").load('/Home/_Home');
+
+    $('#HomeBtn').on('click', function () {
+        $("#myContentView").load('/Home/_Home');
+    });
+
+    $('#SchBtn').on('click', function () {
+        $("#myContentView").load('/Home/Schedule');
+    });
+
+    $('#AboutBtn').on('click', function() {
+        $("#myContentView").load('/Home/About');
+    });
+    
+    $('#ContactBtn').on('click', function() {
+        $("#myContentView").load('/Home/Contact');
+    });
+
+});
